315661c328b3ef9b2b7b4898515923b7e48785a8
imadechumbo morais <weiganlight@gmail.com>
2026-03-30 20:39:42 -0300

    fix: app.js cors global clean

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,35 +1,7 @@
 import express from "express";
 import helmet from "helmet";
 import compression from "compression";
-const allowedOrigins = [
-  "https://technetgame.com.br",
-  "https://www.technetgame.com.br",
-  "https://technetgame-site.pages.dev",
-  "http://localhost:3000",
-  "http://127.0.0.1:3000"
-];
 
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-
-  if (origin && allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Vary", "Origin");
-  }
-
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Refresh-Token, Accept"
-  );
-  res.setHeader("Access-Control-Max-Age", "86400");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
-
-  next();
-});
 import healthRoutes from "./routes/healthRoutes.js";
 import newsRoutes from "./routes/newsRoutes.js";
 import metaRoutes from "./routes/metaRoutes.js";
@@ -61,24 +33,28 @@ app.use((req, res, next) => {
   }
 
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Refresh-Token, Accept");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Refresh-Token, Accept"
+  );
   res.setHeader("Access-Control-Max-Age", "86400");
 
   if (req.method === "OPTIONS") {
     return res.sendStatus(204);
   }
 
-  return next();
+  next();
 });
 
-app.use(helmet({
-  crossOriginEmbedderPolicy: false,
-  contentSecurityPolicy: false,
-  crossOriginResourcePolicy: false,
-}));
+app.use(
+  helmet({
+    crossOriginEmbedderPolicy: false,
+    contentSecurityPolicy: false,
+    crossOriginResourcePolicy: false,
+  })
+);
 
 app.use(compression());
-
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 
